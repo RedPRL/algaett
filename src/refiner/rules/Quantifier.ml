21b3cd3a191117c9bacc55fa3a40c114234621f9
Jon Sterling <jon@jonmsterling.com>
2022-07-06 12:59:57 +0200

    chore: refactor Quantifier.ml

--- src/refiner/rules/Quantifier.ml
+++ src/refiner/rules/Quantifier.ml
@@ -7,30 +7,23 @@ type rule =
   -> (S.t -> S.t -> S.t)
   -> T.check
 
-let quantifier ~name ~cbase ~cfam syn : T.check =
+let auxiliary ~name ~cbase ~cbase_sort ~cfam builder : T.check =
   T.Check.rule @@ fun goal ->
-  match goal.tp with
-  | D.Univ _ ->
-    let base = T.Check.run {tp = goal.tp; lhs = LHS.unknown} cbase in
-    let vbase = Eff.eval base in
-    let fam =
-      Eff.bind ~name ~tp:vbase @@ fun x ->
-      T.Check.run {tp = goal.tp; lhs = LHS.unknown} @@ cfam x
-    in
-    syn base fam
-  | _ ->
-    invalid_arg "quantifier"
+    match goal.tp with
+    | D.Univ _ ->
+      let base = T.Check.run {tp = cbase_sort goal.tp; lhs = LHS.unknown} cbase in
+      let vbase = Eff.eval base in
+      let fam =
+        Eff.bind ~name ~tp:vbase @@ fun x ->
+        T.Check.run {tp = goal.tp; lhs = LHS.unknown} @@ cfam x
+      in
+      builder base fam
+    | _ ->
+      invalid_arg "quantifier"
 
-let vir_quantifier ~name ~cbase ~cfam syn : T.check =
-  T.Check.rule @@ fun goal ->
-  match goal.tp with
-  | D.Univ _ ->
-    let base = T.Check.run {tp = D.VirUniv; lhs = LHS.unknown} cbase in
-    let vbase = Eff.eval base in
-    let fam =
-      Eff.bind ~name ~tp:vbase @@ fun x ->
-      T.Check.run {tp = goal.tp; lhs = LHS.unknown} @@ cfam x
-    in
-    syn base fam
-  | _ ->
-    invalid_arg "quantifier"
+
+let quantifier ~name ~cbase ~cfam builder : T.check =
+  auxiliary ~name ~cbase ~cbase_sort:(fun univ -> univ) ~cfam builder
+
+let vir_quantifier ~name ~cbase ~cfam builder : T.check =
+  auxiliary ~name ~cbase ~cbase_sort:(fun _ -> D.VirUniv) ~cfam builder
